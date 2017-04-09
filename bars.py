98357c2099a3fc6f1c7ce8840c4255961c2ca005
gitmantis <kolesnikov.andrey@gmail.com>
2017-04-10 00:07:24 +0300

    fixed

--- bars.py
+++ bars.py
@@ -1,7 +1,7 @@
 import json
+import sys
 import math
 import os.path
-from operator import itemgetter, attrgetter
 from pprint import pprint
 
 
@@ -13,56 +13,22 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    return max([bar for bar in data], key=lambda d: d['SeatsCount'])    
+    return max(data, key=lambda d: d['SeatsCount'])    
 
 def get_smallest_bar(data):
-    return min([bar for bar in data], key=lambda d: d['SeatsCount'])
+    return min(data, key=lambda d: d['SeatsCount'])
 
 def get_closest_bar(data, longitude, latitude):
-    distance = 0    
-    closest_bar = {}
-    earthradius = 6372795    
-    distance_min = 2*math.pi*earthradius
-   
-    for bar in data:
-        bar_longitude = bar["geoData"]["coordinates"][0]
-        bar_latitude  = bar["geoData"]["coordinates"][1]
- 
-        #перевести координаты в радианы
-        lat1  = bar_latitude * math.pi / 180;
-        long1 = bar_longitude * math.pi / 180;
-        lat2  = latitude * math.pi / 180;
-        long2 = longitude * math.pi / 180;
- 
-        #косинусы и синусы широт и разницы долгот
-        cl1 = math.cos(lat1);
-        cl2 = math.cos(lat2);
-        sl1 = math.sin(lat1);
-        sl2 = math.sin(lat2);
-        delta = long2 - long1;
-        cdelta = math.cos(delta);
-        sdelta = math.sin(delta);
- 
-        #вычисления длины большого круга
-        y = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
-        x = sl1 * sl2 + cl1 * cl2 * cdelta;
- 
-        ad = math.atan2(y, x);
-        distance = ad * earthradius;
-        
-        if distance < distance_min:
-            distance_min = distance
-            closest_bar = bar    
- 
-    return closest_bar
-   
+    return min(data, key=lambda d: math.hypot(longitude - float(d["Longitude_WGS84"]), latitude - float(d["Latitude_WGS84"])))
 
 if __name__ == '__main__':
     fpath  = input("Enter json filepath:")
+    bars_data = load_data(fpath)
+    if bars_data is None:
+        print ("File '%s' not found" % (fpath)) 
+        sys.exit()
     mylat  = float(input("Enter your latitude:"))
     mylong = float(input("Enter your longitude:"))
-
-    bars_data = load_data(fpath)
     print("\nBiggest bar: \n") 
     pprint (get_biggest_bar(bars_data))
     print("\nSmallest bar: \n") 
